3af9f8324230478766058215c261b6891420feaa
HaaniTharak <70282036+HaaniTharak@users.noreply.github.com>
2025-11-16 15:17:07 -0800

    Revert "LETSSSS GOOOO"
    This reverts commit c14a63dfba7acaf870ba5fe43db1181e6255423c.

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./app.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Deal with it",
+  description: "Your One-Stop Shop for the best Deals",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
